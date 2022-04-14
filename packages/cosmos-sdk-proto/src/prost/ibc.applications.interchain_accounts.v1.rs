aed2b309eb07c53021c57ebe9982d301f31e12a4
vuong <nguyenvuong1122000@gmail.com>
2022-04-15 02:31:14 +0700

    refactor code struct

--- packages/cosmos-sdk-proto/src/prost/ibc.applications.interchain_accounts.v1.rs
+++ packages/cosmos-sdk-proto/src/prost/ibc.applications.interchain_accounts.v1.rs
@@ -22,38 +22,6 @@ pub struct Metadata {
     #[prost(string, tag = "6")]
     pub tx_type: ::prost::alloc::string::String,
 }
-/// GenesisState defines the interchain accounts genesis state
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct GenesisState {
-    #[prost(message, optional, tag = "1")]
-    pub controller_genesis_state: ::core::option::Option<ControllerGenesisState>,
-    #[prost(message, optional, tag = "2")]
-    pub host_genesis_state: ::core::option::Option<HostGenesisState>,
-}
-/// ControllerGenesisState defines the interchain accounts controller genesis state
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct ControllerGenesisState {
-    #[prost(message, repeated, tag = "1")]
-    pub active_channels: ::prost::alloc::vec::Vec<ActiveChannel>,
-    #[prost(message, repeated, tag = "2")]
-    pub interchain_accounts: ::prost::alloc::vec::Vec<RegisteredInterchainAccount>,
-    #[prost(string, repeated, tag = "3")]
-    pub ports: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-    #[prost(message, optional, tag = "4")]
-    pub params: ::core::option::Option<super::controller::v1::Params>,
-}
-/// HostGenesisState defines the interchain accounts host genesis state
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct HostGenesisState {
-    #[prost(message, repeated, tag = "1")]
-    pub active_channels: ::prost::alloc::vec::Vec<ActiveChannel>,
-    #[prost(message, repeated, tag = "2")]
-    pub interchain_accounts: ::prost::alloc::vec::Vec<RegisteredInterchainAccount>,
-    #[prost(string, tag = "3")]
-    pub port: ::prost::alloc::string::String,
-    #[prost(message, optional, tag = "4")]
-    pub params: ::core::option::Option<super::host::v1::Params>,
-}
 /// ActiveChannel contains a connection ID, port ID and associated active channel ID
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ActiveChannel {
@@ -99,13 +67,4 @@ pub enum Type {
     Unspecified = 0,
     /// Execute a transaction on an interchain accounts host chain
     ExecuteTx = 1,
-}
-/// An InterchainAccount is defined as a BaseAccount & the address of the account owner on the controller chain
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct InterchainAccount {
-    #[prost(message, optional, tag = "1")]
-    pub base_account:
-        ::core::option::Option<super::super::super::super::cosmos::auth::v1beta1::BaseAccount>,
-    #[prost(string, tag = "2")]
-    pub account_owner: ::prost::alloc::string::String,
 }
