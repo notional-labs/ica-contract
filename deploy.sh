bb59b0f1e76074b5f1df402a2460ab7e632f32e1
Sotatek-HieuVu2 <hieuvubk@gmail.com>
2022-04-15 00:54:10 +0700

    instance contract

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
 
 #NODE="tcp://localhost:2281"
-NODE="https://rpc.castor-1.stargaze-apis.com:443"
-ACCOUNT="hieuvu"
-NFT_CONTRACT_DIR="artifacts/sg721.wasm"
-MINTER_CONTRACT_DIR="artifacts/minter.wasm"
+NODE=""
+ACCOUNT="mykey"
+CONTRACT_DIR="artifacts/ibc_reflect_send.wasm"
 SLEEP_TIME="15s"
-CHAIN_ID="castor-1"
-CONTRACT_ADDRESS="cosmos14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s4hmalr"
+CHAIN_ID="test-1"
 
-#Code id for CW721 contract on Castor testnet, no need to restore
-NFT_CODE_ID="1"
-#Code id for Miner contract on Castor testnet, no need to restore
-MINTER_CODE_ID="2"
 
-INIT='{
-  "base_token_uri": "ipfs://bafybeifieycaar2vgo3loqi3zgkzttsg5amnr2rsplzikg236l2omumsyq/metadata",
-  "num_tokens": 10,
-  "sg721_code_id": 1,
-  "sg721_instantiate_msg": {
-    "name": "Film TOken",
-    "symbol": "FILM",
-    "minter": "stars1gsrq05myhsxsks8mdy4wtm2ep05qk2m8twa0y3",
-    "collection_info": {
-	  "creator": "stars1gsrq05myhsxsks8mdy4wtm2ep05qk2m8twa0y3",
-	  "description":"For test net",
-      "image": "ipfs://QmXG7jzphfwXDw5acwoW8WqAjQKmBVSeEUoUcxyME1rxmG?filename=2020-07-28%2019-23-52_5042.jpg",
-      "royalty_info": {
-        "payment_address": "stars1gsrq05myhsxsks8mdy4wtm2ep05qk2m8twa0y3",
-        "share": "0.1"
-      }
-    }
-  },
-  "per_address_limit": 1,
-  "start_time": "1647501005586088965",
-  "unit_price": {
-    "amount": "100000000",
-    "denom": "ustars"
-  }
-}'
-INIT_JSON=$(starsd tx wasm instantiate "$MINTER_CODE_ID" "$INIT" --from "$ACCOUNT" --label "minter nft" -y --chain-id "$CHAIN_ID" --node "$NODE" --gas 10000000 --amount 1000000000ustars --no-admin -o json)
+RES=$(digd tx wasm store "$CONTRACT_DIR" --from "$ACCOUNT" -y --output json --chain-id "$CHAIN_ID" --gas 35000000 --fees 875000stake -y --output json)
+echo $RES
+
+if [ "$(echo $RES | jq -r .raw_log)" != "[]" ]; then
+	# exit
+	echo "ERROR = $(echo $RES | jq .raw_log)"
+	exit 1
+else
+	echo "STORE SUCCESS"
+fi
+
+TXHASH=$(echo $RES | jq -r .txhash)
+
+echo $TXHASH
+
+sleep for chain to update
+sleep "$SLEEP_TIME"
+
+RAW_LOG=$(digd query tx "$TXHASH" --chain-id "$CHAINID" --node "$NODE" -o json | jq -r .raw_log)
+
+echo $RAW_LOG
+
+CODE_ID=$(echo $RAW_LOG | jq -r .[0].events[1].attributes[0].value)
+
+echo $CODE_ID
+
+INIT_JSON=$(digd tx wasm instantiate 13 {} --from $ACCOUNT --label "instantiate contract" -y --chain-id $CHAIN_ID --gas 10000000 --fees 100000000stake -o json --no-admin)
 
 echo "INIT_JSON = $INIT_JSON"
 
@@ -54,24 +49,11 @@ fi
 # sleep for chain to update
 sleep "$SLEEP_TIME"
 
-RAW_LOG=$(starsd query tx "$(echo $INIT_JSON | jq -r .txhash)" --chain-id "$CHAINID" --node "$NODE" --output json | jq -r .raw_log)
+RAW_LOG=$(digd query tx "$(echo $INIT_JSON | jq -r .txhash)" --chain-id "$CHAINID" --node "$NODE" --output json | jq -r .raw_log)
 
 echo "RAW_LOG = $RAW_LOG"
 
 CONTRACT_ADDRESS=$(echo $RAW_LOG | jq -r .[0].events[0].attributes[0].value)
 
-CONTRACT_DATA=$(starsd query wasm contract "$CONTRACT_ADDRESS" --node "$NODE" -o json)
+echo "CONTRACT ADDRESS = $CONTRACT_ADDRESS"
 
-echo "CONTRACT = $CONTRACT_DATA"
-
-# sleep "$SLEEP_TIME"
-
-# BALANCE="{"balance":{"address":"$(starsd keys show $ACCOUNT -a)"}}"
-
-# BALANCE_QUERY=$(starsd query wasm contract-state smart "$CONTRACT_ADDRESS" "$BALANCE" --node "$NODE" -o json)
-
-# echo "BALANCE = $BALANCE_QUERY"
-
-
-
-# starsd query wasm contract-state smart "$CONTRACT_ADDRESS" '{"list_channels": {}}' --chain-id "$CHAINID" --node "$NODE"
