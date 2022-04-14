aa40e6aa8f120554a4377373d7da35251261b07f
vuong <nguyenvuong1122000@gmail.com>
2022-04-15 02:21:33 +0700

    proto ibc rust

---
+++ packages/cosmos-sdk-proto/src/lib.rs
@@ -0,0 +1,23 @@
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/cosmos/cosmos-rust/main/.images/cosmos.png",
+    html_root_url = "https://docs.rs/cosmos-sdk-proto/0.9.0"
+)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![forbid(unsafe_code)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_import_braces)]
+
+/// IBC protobuf definitions.
+pub mod ibc {
+    /// IBC applications.
+    pub mod applications {
+
+        /// ICA support.
+        pub mod interchain_accounts {
+            pub mod v1 {
+                include!("prost/ibc.applications.interchain_accounts.v1.rs");
+            }
+        }
+    }
+
+}
